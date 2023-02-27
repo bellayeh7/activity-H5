31f6ac1a0ca748239bb311d05616cb2b4a886511
yefang <2363453031@qq.com>
2023-02-27 12:47:19 +0800

    feat: banner初始

---
+++ src/sections/FirstSection/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import BannerImage from '../../assets/banner.png'
+import styles from './styles.module.scss'
+
+
+const FirstSection: FC = () => {
+  return (
+  <div className={styles.firstSection}>
+    <img src={BannerImage} alt="Banner" />
+  </div>
+  );
+};
+
+
+export default FirstSection;
